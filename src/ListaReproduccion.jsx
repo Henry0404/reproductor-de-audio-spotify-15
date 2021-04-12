33b5a6a24edbb14d18e3fe1a1d07c46ec1107743
Bárbara Ulloa <ulloaby@gmail.com>
2021-04-11 22:46:58 -0400

    proyecto con botones funcionando y terminado

--- src/ListaReproduccion.jsx
+++ src/ListaReproduccion.jsx
@@ -1,63 +1,86 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { canciones } from "./canciones.js";
+import Controles from "./components/Controles";
 
 function ListaReproduccion() {
   const [reproduciendo, setReproduciendo] = useState(false);
-  const [cancionUrl, setCancionUrl] = useState("");
-  const audioRef = useRef();
+  const [indexActual, setIndexActual] = useState(0);
+  let audioRef = useRef();
 
-  function reproducirCancion(cancion) {
+  const reproducirCancion = (indexCancion) => {
+    let cancion = canciones[indexCancion];
+    audioRef.src = `https://assets.breatheco.de/apis/sound/${cancion.url}`;
+    setIndexActual(indexCancion);
     setReproduciendo(true);
-    setCancionUrl(cancion.url);
-    audioRef.current.play();
-  }
+  };
 
-  function pausarReproduccion() {
-    setReproduciendo(false);
-    audioRef.current.pause();
-  }
+  const alternarReproduccion = () => {
+    if (audioRef.paused) {
+      audioRef.play();
+      setReproduciendo(true);
+    } else {
+      audioRef.pause();
+      setReproduciendo(false);
+    }
+  };
+
+  const siguienteCancion = () => {
+    let nuevoIndex;
+
+    if (indexActual < canciones.length - 1) {
+      nuevoIndex = indexActual + 1;
+    } else {
+      nuevoIndex = 0;
+    }
+
+    reproducirCancion(nuevoIndex);
+  };
+
+  const cancionAnterior = () => {
+    let nuevoIndex;
+
+    if (indexActual === 0) {
+      nuevoIndex = canciones.length - 1;
+    } else {
+      nuevoIndex = indexActual - 1;
+    }
+
+    reproducirCancion(nuevoIndex);
+  };
 
   return (
     <div className="container bg-dark">
-      <audio
-        ref={audioRef}
-        src={"https://assets.breatheco.de/apis/sound/" + cancionUrl}
-      />
       <div className="card text-center bg-dark">
         <div className="card-header bg-dark text-white bg-dark">
           Lista de Reproducción
+          <audio
+            className="audioControl"
+            ref={(r) => (audioRef = r)}
+            autoPlay
+          />
         </div>
+
         <ul className="list-group list-group-flush bg-dark">
-          {canciones.map((item, index) => {
+          {canciones.map((cancion, index) => {
             return (
               <button
                 key={index}
-                onClick={() => reproducirCancion(item)}
+                onClick={() => reproducirCancion(index)}
                 type="button"
                 className="list-group-item list-group-item-action list-group-item-dark"
               >
-                {item.name}
+                {cancion.name}
               </button>
             );
           })}
         </ul>
-        <div className="container card-footer bg-dark">
-          <button>
-            <i className="fas fa-arrow-alt-circle-left"></i>
-          </button>
-          {reproduciendo === true ? (
-            <button onClick={() => pausarReproduccion()}>
-              <i className="fas fa-pause-circle"></i>
-            </button>
-          ) : (
-            <button>
-              <i className="fas fa-play-circle"></i>
-            </button>
-          )}
-          <button>
-            <i className="fas fa-arrow-alt-circle-right"></i>
-          </button>
-        </div>
+
+        <Controles
+          reproduciendo={reproduciendo}
+          alternarReproduccion={alternarReproduccion}
+          cancionAnterior={cancionAnterior}
+          siguienteCancion={siguienteCancion}
+        />
       </div>
     </div>
   );
--- src/canciones.js
+++ src/canciones.js
@@ -18,99 +18,58 @@ export let canciones = [
         "url": "files/mario/songs/overworld.mp3"
     },
     {
-        "id": 4,
+        "id": 1,
         "category": "game",
-        "name": "Mario Stage 1",
-        "url": "files/mario/songs/stage1.mp3"
+        "name": "Mario Castle",
+        "url": "files/mario/songs/castle.mp3"
     },
     {
-        "id": 5,
-        "category": "game",
-        "name": "Mario Stage 2",
-        "url": "files/mario/songs/stage2.mp3"
-    },
-    {
-        "id": 6,
+        "id": 2,
         "category": "game",
         "name": "Mario Star",
-        "url": "files/mario/songs/starman.mp3"
+        "url": "files/mario/songs/hurry-starman.mp3"
     },
     {
-        "id": 7,
+        "id": 3,
         "category": "game",
-        "name": "Mario Underworld",
-        "url": "files/mario/songs/underworld.mp3"
-    },
-    {
-        "id": 8,
+        "name": "Mario Overworld",
+        "url": "files/mario/songs/overworld.mp3"
+    }, {
+        "id": 1,
         "category": "game",
-        "name": "Mario Underwater",
-        "url": "files/mario/songs/underwater.mp3"
+        "name": "Mario Castle",
+        "url": "files/mario/songs/castle.mp3"
     },
     {
-        "id": 9,
+        "id": 2,
         "category": "game",
-        "name": "Zelda Castle",
-        "url": "files/videogame/songs/zelda_castle.mp3"
+        "name": "Mario Star",
+        "url": "files/mario/songs/hurry-starman.mp3"
     },
     {
-        "id": 10,
+        "id": 3,
         "category": "game",
-        "name": "Zelda Outworld",
-        "url": "files/videogame/songs/zelda_outworld.mp3"
-    },
-    {
-        "id": 11,
+        "name": "Mario Overworld",
+        "url": "files/mario/songs/overworld.mp3"
+    }, {
+        "id": 1,
         "category": "game",
-        "name": "Zelda Titles",
-        "url": "files/videogame/songs/zelda_title.mp3"
+        "name": "Mario Castle",
+        "url": "files/mario/songs/castle.mp3"
     },
     {
-        "id": 11,
+        "id": 2,
         "category": "game",
-        "name": "Sonic Brain Zone",
-        "url": "files/videogame/songs/sonic_brain-zone.mp3"
+        "name": "Mario Star",
+        "url": "files/mario/songs/hurry-starman.mp3"
     },
     {
-        "id": 11,
+        "id": 3,
         "category": "game",
-        "name": "Zelda Link To Past",
-        "url": "files/videogame/songs/zelda_link-to-past.mp3"
+        "name": "Mario Overworld",
+        "url": "files/mario/songs/overworld.mp3"
     },
-    {
-        "id": 15,
-        "game": "cartoon",
-        "name": "Flintstones",
-        "url": "files/cartoons/songs/flintstones.mp3"
-    },
-    {
-        "id": 16,
-        "game": "cartoon",
-        "name": "power-rangers",
-        "url": "files/cartoons/songs/power-rangers.mp3"
-    },
-    {
-        "id": 17,
-        "game": "cartoon",
-        "name": "simpsons",
-        "url": "files/cartoons/songs/simpsons.mp3"
-    },
-    {
-        "id": 18,
-        "game": "cartoon",
-        "name": "south-park",
-        "url": "files/cartoons/songs/south-park.mp3"
-    },
-    {
-        "id": 19,
-        "game": "cartoon",
-        "name": "thundercats",
-        "url": "files/cartoons/songs/thundercats.mp3"
-    },
-    {
-        "id": 20,
-        "game": "cartoon",
-        "name": "x-men",
-        "url": "files/cartoons/songs/x-men.mp3"
-    }
+
 ];
+
+
